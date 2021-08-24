ea9d2084c1596de4df0d822a64e8851510da0d94
diegopa <diego.loge@hotmail.com>
2021-08-24 00:59:53 -0500

    arreglando formato de salida

--- app/Http/Controllers/VigenereController.php
+++ app/Http/Controllers/VigenereController.php
@@ -10,90 +10,67 @@ class VigenereController extends Controller
         return view('index');
     }
 
-    function cifrar(Request $request)
-    {
+    function cifrar(Request $request){
+
         $key = $request->key;
         $cadena = $request->cadena;
         $key = strtolower($key);
-        
-
-        $code = "";
         $indiceLlave = 0;
         $longitudLlave = strlen($key);
         $longitudCadena = strlen($cadena);
-        
 
-        for ($i = 0; $i < $longitudCadena; $i++)
-        {
+        for ($i = 0; $i < $longitudCadena; $i++){
 
-            if (ctype_alpha($cadena[$i]))
-            {
+            if (ctype_alpha($cadena[$i])){
                 
-                if (ctype_upper($cadena[$i]))
-                {
+                if (ctype_upper($cadena[$i])){
                     $cadena[$i] = chr(((ord($key[$indiceLlave]) - ord("a") + ord($cadena[$i]) - ord("A")) % 26) + ord("A"));
-                }
-                
-                
-                else
-                {
+                }else{
                     $cadena[$i] = chr(((ord($key[$indiceLlave]) - ord("a") + ord($cadena[$i]) - ord("a")) % 26) + ord("a"));
                 }
                 
-                
                 $indiceLlave++;
-                if ($indiceLlave >= $longitudLlave)
-                {
+                if ($indiceLlave >= $longitudLlave){
                     $indiceLlave = 0;
                 }
             }
         }
-        
-        
-        dd("La clave cifrada es: ", $cadena);
+        dd("La clave cifrada es: $cadena");
     }
 
 
-    function descifrar(Request $request)
-    {
+    function descifrar(Request $request){
+
         $key = $request->key;
         $cadena = $request->cadena;
         $key = strtolower($key);
-        
-        
-        $code = "";
         $indiceLlave = 0;
         $longitudLlave = strlen($key);
         $longitudCadena = strlen($cadena);
         
         
-        for ($i = 0; $i < $longitudCadena; $i++)
-        {
+        for ($i = 0; $i < $longitudCadena; $i++){
             
-            if (ctype_alpha($cadena[$i]))
-            {
+            if (ctype_alpha($cadena[$i])){
                 
-                if (ctype_upper($cadena[$i]))
-                {
+                if (ctype_upper($cadena[$i])){
+
                     $x = (ord($cadena[$i]) - ord("A")) - (ord($key[$indiceLlave]) - ord("a"));
                     
-                    if ($x < 0)
-                    {
+                    if ($x < 0){
+
                         $x += 26;
                     }
                     
                     $x = $x + ord("A");
                     
                     $cadena[$i] = chr($x);
-                }
-                
-                
-                else
-                {
+
+                }else{
+
                     $x = (ord($cadena[$i]) - ord("a")) - (ord($key[$indiceLlave]) - ord("a"));
                     
-                    if ($x < 0)
-                    {
+                    if ($x < 0){
                         $x += 26;
                     }
                     
@@ -102,17 +79,12 @@ class VigenereController extends Controller
                     $cadena[$i] = chr($x);
                 }
                 
-                
                 $indiceLlave++;
-                if ($indiceLlave >= $longitudLlave)
-                {
+                if ($indiceLlave >= $longitudLlave){
                     $indiceLlave = 0;
                 }
             }
         }
-        
-        
-        dd("La clave descifrada es: ", $cadena);
-
-    } 
+        dd("La clave descifrada es: $cadena");
+    }
 }
